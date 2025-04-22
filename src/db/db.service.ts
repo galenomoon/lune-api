8b1c099373389147164a58e7db0ac985358f42ab
galenomoon <galeno.dev@gmail.com>
2025-04-22 13:19:16 -0300

    Comment out password hashing logic in DBService fix method

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -8,18 +8,5 @@ export class DBService {
   constructor(private readonly prisma: PrismaService) {}
 
   async fix() {
-    const teachers = await this.prisma.teacher.findMany();
-
-    for (const teacher of teachers) {
-      if (teacher?.cpf) {
-        const salt = crypto.randomInt(0, 10);
-        const hashed_password = await bcrypt.hash(teacher.cpf.replace(/\D/g, '')?.slice(0, 4), salt);
-    
-        await this.prisma.teacher.update({
-          where: { id: teacher.id },
-          data: { password: hashed_password },
-        });
-      }
-    }
   }
 }
